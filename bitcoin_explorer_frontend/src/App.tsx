5ef1ce8115bcecee681e1d37211b9cfa64182d0f
siruiwei <wei.sir@northeastern.edu>
2024-06-13 21:16:43 -0700

    front end week2 complete

--- bitcoin_explorer_frontend/src/App.tsx
+++ bitcoin_explorer_frontend/src/App.tsx
@@ -1,9 +1,16 @@
 import { Blocks } from "@/components/Blocks.tsx"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Price_Graph from "./components/Price_Graph";
 
 function App() {
   return (
     <div>
-      <Blocks></Blocks>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Blocks />} />
+          <Route path="/price" element={<Price_Graph />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
