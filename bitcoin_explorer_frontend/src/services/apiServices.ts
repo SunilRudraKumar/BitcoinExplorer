bf9eabc708a40fa6beeeb375e6b36fdfc7dd3420
siruiwei <wei.sir@northeastern.edu>
2024-06-19 17:02:13 -0700

    api updates

--- bitcoin_explorer_frontend/src/services/apiServices.ts
+++ bitcoin_explorer_frontend/src/services/apiServices.ts
@@ -3,13 +3,15 @@ import axios from 'axios';
 const API_URL = 'http://localhost:3001';
 
 export const getOnchainData = async () => {
-  const response = await axios.get(`${API_URL}/onchain`);
-  console.log("From Services ", response)
-  return response.data;
+  // const response = await axios.get(`${API_URL}/onchain`);
+  // console.log("From Services ", response)
+  // return response.data;
+  return {data: "1,2,3,4"};
 };
 
 export const getOffchainData = async () => {
-  const response = await axios.get(`${API_URL}/offchain`);
-  console.log("From Services ", response)
-  return response.data;
+  // const response = await axios.get(`${API_URL}/offchain`);
+  // console.log("From Services ", response)
+  // return response.data;
+  return {data: "1,2,3,4"};
 };
